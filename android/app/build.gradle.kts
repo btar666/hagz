b113b14780ec4d2d91098fcad2deca69ab1405b7
btar <btar666@example.com>
2025-11-17 23:21:42 +0300

    ok

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.hagz"
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
